50e06d720bdc9f01c0900c3ff15764195ee947da
Gvzum <diasgazim@icloud.com>
2022-03-02 20:59:38 +0600

    Adding requirements file to manage modules.

--- Qrep/bastama/views.py
+++ Qrep/bastama/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+app_name = 'bastama'
+
 def index(request):
     return render(request, 'bastama/index.html')
+
+def jeans(request):
+    return render(request, 'bastama/jeans.html')
