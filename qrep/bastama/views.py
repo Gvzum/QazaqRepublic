2aab4955873e713413d37e6cdcc6233a8fa82f6f
Gvzum <diasgazim@icloud.com>
2022-03-24 23:02:46 +0600

    Updating models, adding some models. And changing urls and views.

--- qrep/bastama/views.py
+++ qrep/bastama/views.py
@@ -3,74 +3,12 @@ from django.views.generic import *
 from django.shortcuts import render
 
 from .models import *
+from .utils import SIDEBAR_SUBCATEGORY_PRODUCTS, basket_data
 
 app_name = 'bastama'
 
 def index(request):
     return render(request, 'bastama/index.html', {'title': 'Qazaq Republic'})
-#
-# def jeans(request):
-#     return render(request, 'bastama/jeans.html')
-
-# def jeide(request):
-#     return render(request, 'bastama/jeide.html')
-
-class JeansView(ListView):
-    model = Product
-    template_name = 'bastama/jeans.html'
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Jeansilar'
-        context['jeans'] = Product.objects.filter(category__name='Jeans')
-        context['shalbars'] = Product.objects.filter(category__name='Shalbar')
-
-        return context
-
-def get_jean(request, slug):
-    jean = Product.objects.get(slug=slug)
-    print(jean)
-    return HttpResponse('Jeans are there')
-
-# class GetOneJeansView(DetailView):
-#     model = Product
-
-class JeideView(ListView):
-    model = Product
-    template_name = 'bastama/jeide.html'
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Jeideler'
-        context['jeideler'] = Product.objects.filter(category__name='Jeide')
-        context['kenjeideler'] = Product.objects.filter(category__name='Kenjeide')
-        context['birjeideler'] = Product.objects.filter(category__name='Burjeide')
-
-        return context
-
-class QosymshaView(ListView):
-    model = Product
-    template_name = 'bastama/qosymsha.html'
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Qosymsha'
-        context['betperdeler'] = Product.objects.filter(category__name='Betperde')
-        context['botelkeler'] = Product.objects.filter(category__name='Botelke')
-        context['keseler'] = Product.objects.filter(category__name='Kese')
-        context['qalpaktar'] = Product.objects.filter(category__name='Qalpaq')
-        context['panamalar'] = Product.objects.filter(category__name='Panama')
-        context['kepkalar'] = Product.objects.filter(category__name='Kepka')
-        context['somkeler'] = Product.objects.filter(category__name='Somke')
-
-        return context
-
-
-# def qosymsha(request):
-#     return render(request, 'bastama/qosymsha.html')
-
-def gift(request):
-    return render(request, 'bastama/gift.html')
 
 def basket(request):
     return render(request, 'bastama/basket.html')
@@ -81,12 +19,45 @@ def search(request):
     }
     if request.method == 'POST':
         context['products'] = Product.objects.filter(name__contains=request.POST['search'], category__name__contains=request.POST['search'])
-        print(context['products'])
 
     return render(request, 'bastama/search.html', context)
 
 def lookbook(request):
     return render(request, 'bastama/lookbook.html')
 
-def test(request):
-    return render(request, 'bastama/components/test.html')
+def show_category_products(request, cat_name):
+
+    basket_data(request)
+
+    context = {}
+    template_url = 'bastama/'
+    products_from_subcategory = SIDEBAR_SUBCATEGORY_PRODUCTS[cat_name]
+
+    if cat_name == 'jeans':
+        template_url += 'jeans.html'
+    elif cat_name == 'jeide':
+        template_url += 'jeide.html'
+    elif cat_name == 'qosymsha':
+        template_url += 'qosymsha.html'
+    elif cat_name == 'gift':
+        template_url += 'gift.html'
+
+    for product_tupple in products_from_subcategory:
+        context_key = product_tupple[0]
+        value_to_filter = product_tupple[1]
+        context['title'] = value_to_filter
+        context[context_key] = Product.objects.filter(category__name=value_to_filter)
+
+    return render(request, template_url, context)
+
+def product_detail(request, slug):
+    print(slug)
+    product = Product.objects.get(slug=slug)
+    print(product)
+    return HttpResponse('There you are')
+
+def get_favorite_products(request):
+    print(request.user)
+    favs = Favors.objects.filter(customer=request.user)
+    print(favs)
+    return HttpResponse('Favorite page')
