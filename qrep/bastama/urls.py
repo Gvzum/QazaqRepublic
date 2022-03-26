5eec85f6f13a51a5c3c06db8003f95137d325afc
Gvzum <diasgazim@icloud.com>
2022-03-26 15:10:54 +0600

    Adding favorite products logic.

--- qrep/bastama/urls.py
+++ qrep/bastama/urls.py
@@ -6,9 +6,11 @@ urlpatterns = [
     path('category/<str:cat_name>/', category_products, name='category_products'),
     path('product/<slug:slug>/', product_detail, name='product_detail'),
 
+    path('favorite/', favorite_products, name='favorite'),
     path('basket/', basket, name='basket'),
     path('lookbook/', lookbook, name='lookbook'),
     path('search/', search, name='search'),
 
-    path('favorite/', favorite_products, name='favorite')
+    path('test/<slug:slug>/', test, name='test'),
+    path('update_favs/<slug:slug>/', click_like, name='click_like')
 ]
